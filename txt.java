5b729698763329d3ebd3d83e35191d8ff9d07a80
sanjayvishal <sanjayvishal765413@gmail.com>
2025-08-06 15:28:46 +0530

    Update txt.java

--- txt.java
+++ txt.java
@@ -1,1 +1,22 @@
+public class Student {
+    private String name;
+    private int studentId;
+
+    
+    public Student(String name, int studentId) {
+        this.name = name;
+        this.studentId = studentId;
+    }
+
+    public void displayInfo() {
+        System.out.println("Student Name: " + name);
+        System.out.println("Student ID: " + studentId);
+    }
+
+    public static void main(String[] args) {
+        Student student1 = new Student("Alice", 1001);
+
+        student1.displayInfo();
+    }
+}
 
